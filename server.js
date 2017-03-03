554bf51048d18d569e518b23aeb254b6d7509fd6
oumike <michael.cojocari@gmail.com>
2017-03-02 21:09:17 -0500

    Added some route stuff.

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
+const util = require('util')
+
 var port = process.env.PORT || 3000
+
+// Models
+var Categories = require('./models/categories');
+var Quotes = require('./models/quotes');
+var Authors = require('./models/authors');
+var Publishers = require('./models/publishers');
 
 var router = express.Router()
 
 router.use(function(res, req, next) {
-    console.log("Access")
+    console.log('Someone has used the API.')
     next()
 })
 
@@ -20,6 +28,52 @@ router.use(function(res, req, next) {
 router.get('/', function(req, res) {
     res.json({ message: 'Test this.' })
 })
+
+// BEGIN - Public Categories
+router.route('/categories')
+
+	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	.get(function(req, res) {
+		Categories.find(function(err, categories) {
+			if (err)
+				res.send(err);
+
+			res.json(categories);
+		});
+	});
+
+router.route('/quotes')
+
+	.get(function(req, res) {
+		Quotes.find(function(err, quotes) {
+			if (err)
+				res.send(err);
+
+			res.json(quotes);
+		})
+	});
+
+router.route('/authors')
+	.get(function(req, res) {
+		Authors.find(function(err, authors) {
+			if(err)
+				res.send(err);
+
+			res.json(authors);
+		});
+	});
+
+router.route('/publishers')
+	.get(function(req, res) {
+		Publishers.find(function(err, publishers) {
+			if(err)
+				res.send(err);
+
+			res.json(authors);
+		});
+	});
+// END - Public Categories
+
 
 app.use('/api', router);
 
