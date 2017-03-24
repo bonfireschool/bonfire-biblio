3b145ba451a18925be459baf65074905d1cced51
oumike <michael.cojocari@gmail.com>
2017-03-24 17:51:18 -0400

    Messing around with the routes, moved it out of server.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 
-var express = require('express')
-var app = express()
+var app = require('express')()
 var bodyParser = require('body-parser')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
@@ -10,70 +8,7 @@ const util = require('util')
 
 var port = process.env.PORT || 3000
 
-// Models
-var Categories = require('./models/category');
-var Quotes = require('./models/quote');
-var Authors = require('./models/author');
-var Publishers = require('./models/publisher');
-
-var router = express.Router()
-
-router.use(function(res, req, next) {
-    console.log('Someone has used the API.')
-    next()
-})
-
-
-// Our routes
-router.get('/', function(req, res) {
-    res.json({ message: 'Test this.' })
-})
-
-// BEGIN - Public Categories
-router.route('/categories')
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Categories.find(function(err, categories) {
-			if (err)
-				res.send(err);
-
-			res.json(categories);
-	});
-});
-
-router.route('/quotes')
-
-	.get(function(req, res) {
-		Quotes.find(function(err, quotes) {
-			if (err)
-				res.send(err);
-
-			res.json(quotes);
-	})
-});
-
-router.route('/authors')
-	.get(function(req, res) {
-		Authors.find(function(err, authors) {
-			if(err)
-				res.send(err);
-
-			res.json(authors);
-	});
-});
-
-router.route('/publishers')
-	.get(function(req, res) {
-		Publishers.find(function(err, publishers) {
-			if(err)
-				res.send(err);
-
-			res.json(authors);
-	});
-});
-// END - Public Categories
-
+var router = require('./routes');
 
 app.use('/', router);
 
